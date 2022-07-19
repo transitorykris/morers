a6472d98a57bb533f00134c895ad991000dbd91a
Kris Foster <kris.foster@gmail.com>
2022-07-19 15:50:53 -0700

    ugly handling of stdin

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,37 @@
 use std::env;
 use std::fs::File;
+use std::io;
 use std::io::BufReader;
 use std::io::prelude::*;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let file = File::open(&args[2]).unwrap();   // TODO check for errors
+    let file: File;
+
+    if args.len() < 2 {
+        // This case is really for handling stdin
+        loop {
+            let mut input = String::new();
+            match io::stdin().read_line(&mut input) {
+                Ok(0) => {
+                    return // EOF we're done
+                }
+                Ok(_) => {
+                    println!("{}", input);
+                }
+                Err(_) => {
+                    panic!("something went wrong!");
+                }
+            }
+        }
+    } else {
+        // attempt to read from the path given
+        // XXX handling a raw string here is probably not what we want to do, use path?
+        file = File::open(&args[1]).unwrap();   // TODO check for errors
+
+    }
+
     let mut buf_reader = BufReader::new(file);
 
     loop {
