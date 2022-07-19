63d0f51a735c6d8fb7273b5d4af9f35c0d19268c
Kris Foster <kris.foster@gmail.com>
2022-07-19 16:01:50 -0700

    Creat a single reader for either stdin or file

--- src/main.rs
+++ src/main.rs
@@ -5,38 +5,22 @@ use std::io::BufReader;
 use std::io::prelude::*;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    let file_name = env::args().nth(1);
 
-    let file: File;
-
-    if args.len() < 2 {
-        // This case is really for handling stdin
-        loop {
-            let mut input = String::new();
-            match io::stdin().read_line(&mut input) {
-                Ok(0) => {
-                    return // EOF we're done
-                }
-                Ok(_) => {
-                    println!("{}", input);
-                }
-                Err(_) => {
-                    panic!("something went wrong!");
-                }
-            }
+    let mut reader: Box<dyn BufRead> = match file_name {
+        None => {
+            // use stdin
+            Box::new(BufReader::new(io::stdin()))
         }
-    } else {
-        // attempt to read from the path given
-        // XXX handling a raw string here is probably not what we want to do, use path?
-        file = File::open(&args[1]).unwrap();   // TODO check for errors
-
-    }
-
-    let mut buf_reader = BufReader::new(file);
+        Some(file_name) => {
+            // use this file
+            Box::new(BufReader::new(File::open(file_name).unwrap())) // XXX need to check for errors!
+        }
+    };
 
     loop {
         let mut contents = String::new();   // may be faster to clear it than allocate new?
-        match buf_reader.read_line(&mut contents) {
+        match reader.read_line(&mut contents) {
             Ok(0) => {
                 return // we're done
             }
