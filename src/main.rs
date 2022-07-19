5bd8f01a6a01d1ddb9bbe6a4b0a5a11dc75d9d1c
Kris Foster <kris.foster@gmail.com>
2022-07-19 15:26:38 -0700

    A trivial cat implementation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::fs::File;
+use std::io::BufReader;
+use std::io::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let file = File::open("/users/krisfoster/Desktop/jane.txt").unwrap();   // TODO check for errors
+    let mut buf_reader = BufReader::new(file);
+
+    loop {
+        let mut contents = String::new();   // may be faster to clear it than allocate new?
+        match buf_reader.read_line(&mut contents) {
+            Ok(0) => {
+                return // we're done
+            }
+            Ok(_) => {
+                println!("{}", contents);   // TODO: a better print, there will be two new lines
+            }
+            Err(_) => {
+                panic!("something went wrong");
+            }
+        }  // Note: we can panic if pipe breaks, how to catch?
+    }
 }
